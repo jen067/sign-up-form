9b2424a19b10f1fffdd706ade2224cef448096cc
jen067 <dudu671194@gmail.com>
2024-09-27 10:58:56 +0800

    新增按鈕判斷條件

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
 function styleChange() {
-  if (this.value != /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/) {
+  if (email.value.search(emailRule) != -1) {
+    console.log(true);
+    valid.style.visibility = "hidden";
+    email.style.borderColor = "hsl(234, 29%, 20%)";
+    email.style.outlineColor = "hsl(234, 29%, 20%)";
+    email.style.backgroundColor = "#fff";
+    email.style.color = "black";
+    btn.forEach((e) => {
+      e.addEventListener("click", btnClick);
+    });
+  } else {
+    console.log(false);
     valid.style.visibility = "visible";
     email.style.outlineColor = "hsl(4, 100%, 67%)";
     email.style.borderColor = "hsl(4, 100%, 67%)";
     email.style.backgroundColor = "hsl(355, 100%, 93%)";
     email.style.color = "hsl(4, 100%, 67%)";
-  } else {
-    valid.style.visibility = "hidden";
   }
 }
 
+function btnClick() {
+  if (main.classList.contains("visible")) {
+    main.classList.remove("visible");
+    vertified.classList.add("visible");
+  } else {
+    main.classList.add("visible");
+    vertified.classList.remove("visible");
+  }
+  form.reset();
+}
+
+const main = document.querySelector("main");
 const valid = document.querySelector(".valid-txt");
+const btn = document.querySelectorAll(".btn");
+let form = document.querySelector("#form");
+let vertified = document.querySelector(".vertified");
 let email = document.querySelector("#email");
+let emailRule =
+  /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/;
 
 email.addEventListener("change", styleChange);
 email.addEventListener("focus", styleChange);
